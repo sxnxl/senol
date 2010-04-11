e11e9b83905bf740d8d2c017b0f888092c3b7e2c
senol.korkmaz <senol.korkmaz@743634cd-a39c-0a1b-9237-8b8c965d5723>
2010-04-11 19:10:38 +0000

    completed and tested

--- code/short/turing/c/turing.c
+++ code/short/turing/c/turing.c
@@ -1,14 +1,13 @@
-#include<stdio.h>/*mail @senolkorkmaz.info*/
-#include <stdlib.h>/* © 2010 Şenol Korkmaz*/
-#define W for(rewind(F);/*License:GNU LGPL*/\
-fscanf(F,"%d %d %d %d %c",&s,&r,&n,&p,&h)>0;)
-#define R(B) if(!(t->p[B])){t->p[B]=malloc( \
-sizeof(Q));t->p[B]->p[B]=0;t->p[B]->p[!B]=t;}
-typedef struct q{struct q*p[2];int di[2];}Q;main(
-int i,char**v){Q*m,*T,*t=malloc(sizeof(Q));
-FILE*F=fopen(*++v,"r"),*G=fopen(*++v,"r");
-int s,r,n,p,h;T=t;i=0;W i=i>s?i:s;m=calloc(i,
-sizeof(Q));W{m[s].p[r]=h=='H'?0:m+n;m[s].d[r]
-=2*(h=='R')+p;};T->p[0]=T->p[1]=0;while((h=
-getc(G))=='1'||h=='0'){R(1)t->d[0]=(h=='1');t
-=t->p[1];}}
+#define W for(rewind(F);fscanf(F,"%d %d %d %d %c",&s,&r,&n,&p,&h)>0;)
+#include<stdlib.h>/* Copying and distribution of this file, with   */
+#include<stdio.h> /* or without modification, are permitted in any */
+#define C(N,S) N  /* medium provided this notice is preserved.     */\
+=calloc(S,sizeof(Q));for(j=0;j<S;){N[j].p[0]=N[j].p[1]=0;N[j++].d=0;}
+typedef struct q{struct q*p[2];int d;}Q;int main(int i,char**v){int s,
+r,n,p,h,j;FILE*F=fopen(v[1],"r"),*G=fopen(v[2],"r");Q*M,*m,*T,*C(t,1)T
+=t;i=0;W i=i>s?i:s;C(M,i)W{M[s].p[r]=h=='H'?0:M+n;M[s].d+=(2*(h=='R')+
+p)<<(2*r);}while((h=fgetc(G))=='1'||h=='0'){if(!(t->p[1])){C(t->p[1],1
+)t->p[1]->p[0]=t;}t->d=(h=='1');t=t->p[1];}t=T;m=M;for(;m!=0;){r=t->d;
+h=((m->d)>>(r*2+1))&1;t->d=((m->d)>>(r*2))&1;if(!(t->p[h])){C(t->p[h],
+1)t->p[h]->p[!h]=t;}t=t->p[h];m=m->p[r];}t=T;while(t){printf("%d",t->d
+);t=t->p[1];}return 0;}/* © 2010 John Doe & Sarı Çizmeli Mehmet Ağa */
