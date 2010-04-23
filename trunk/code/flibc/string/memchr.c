fbfc956a3bdfeb5ce2f2da8b933b7d95aaa0dcd7
senol.korkmaz <senol.korkmaz@743634cd-a39c-0a1b-9237-8b8c965d5723>
2010-04-23 12:04:14 +0000

    added memchr() to flibc

---
+++ trunk/code/flibc/string/memchr.c
@@ -0,0 +1,15 @@
+#include <fstring.h>
+#include <ffake.h>
+
+/* returns a pointer to the first occurrence of         */
+/* c in the first n bytes of s, or NULL if not found    */
+void *
+_f_memchr(const void *s, char c, size_t n)
+{
+  while(n--)
+    {
+      if (*s++ == c)
+        break;
+    }
+  return (void *) (n==0 ? NULL : s);
+}
