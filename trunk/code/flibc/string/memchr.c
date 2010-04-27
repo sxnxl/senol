6ded7822f57b6e8090fb64620ae2087997f54fb5
senol.korkmaz <senol.korkmaz@743634cd-a39c-0a1b-9237-8b8c965d5723>
2010-04-27 15:37:02 +0000

    test svn Id keyword

--- trunk/code/flibc/string/memchr.c
+++ trunk/code/flibc/string/memchr.c
@@ -16,6 +16,7 @@
     You should have received a copy of the GNU Lesser General Public License
     along with flibc.  If not, see <http://www.gnu.org/licenses/>.
 */
+/* $Id$ */
 
 #include "fstring.h"
 #include "ffake.h"
