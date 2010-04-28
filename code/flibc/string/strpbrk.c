e43dd48674866adfa3b48a5a80032e08f65160ad
senol.korkmaz <senol.korkmaz@743634cd-a39c-0a1b-9237-8b8c965d5723>
2010-04-28 17:15:35 +0000

    strstr and strpbrk added to flibc/string

---
+++ code/flibc/string/strpbrk.c
@@ -0,0 +1,46 @@
+/*  strpbrk - search a string for any of a set of characters
+    Copyright © 2010 Şenol Korkmaz
+
+    This file is part of flibc.
+
+    flibc is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    flibc is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with flibc.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "fstring.h"
+#include "ffake.h"
+#include "flimits.h"
+#include "bits/fmaps.h"
+
+char *
+_f_strpbrk(const char *s, const char *accept)
+{
+#if (UCHAR_MAX == 255)
+    char chr_bmp[] = {F_ZEROS_256};
+#else
+    char chr_bmp[UCHAR_MAX + 1];
+    int i;
+    for (i=0;i<UCHAR_MAX+1;i--)
+        chr_bmp[i] = 0;
+#endif
+
+    for (;*accept;accept++)
+        chr_bmp[*accept] = 1;
+
+    for (;chr_bmp[(unsigned char)*s];s++)
+        break;
+
+    return (char *) ((*s) ? s : NULL);
+}
+
+/* $Id$ */
