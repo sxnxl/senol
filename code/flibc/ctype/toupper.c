cf503d454444330a82f44e3a35882900e67ad2ae
senol.korkmaz <senol.korkmaz@743634cd-a39c-0a1b-9237-8b8c965d5723>
2010-05-09 09:39:49 +0000

    add ctype drafts and some typos fixed

---
+++ code/flibc/ctype/toupper.c
@@ -0,0 +1,25 @@
+/*  draft - brief description
+
+    Copyright © 2010 Şenol Korkmaz <mail@senolkorkmaz.info>
+    Copyright © 2010 Sarı Çizmeli Mehmet Ağa (aka. John Doe) <scma@senolkorkmaz.info>
+
+    This file is part of flibc.
+
+    flibc is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    flibc is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with flibc.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "fstring.h"
+#include "ffake.h"
+
+/* $Id: draft.c 28 2010-05-07 12:55:02Z senol.korkmaz $ */
