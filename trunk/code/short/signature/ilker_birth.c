511c7bb95c9a41d428d0d3c32c8a83a38edc34a7
senol.korkmaz <senol.korkmaz@743634cd-a39c-0a1b-9237-8b8c965d5723>
2010-06-26 14:32:45 +0000

    folder signature/ moved to short/ and turing.py indented

---
+++ trunk/code/short/signature/ilker_birth.c
@@ -0,0 +1,10 @@
+typedef char* happy;main(){
+
+  long long *live = "İlker";
+  happy birthday = "İlker";
+
+  while (*birthday++) printf (
+
+  "Mutlu yıllar %s\a\n\a"     ,
+  (happy) live               );
+  return birthday            ;}
